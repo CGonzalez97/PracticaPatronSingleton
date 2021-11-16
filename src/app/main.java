dbd1da15d6fda0057fa4430d792f3007577082f4
CGonzalez97 <cgl76490@gmail.com>
2021-11-16 11:37:52 +0100

    Subida

---
+++ src/app/main.java
@@ -0,0 +1,21 @@
+package app;
+
+import app.model.Persona;
+import app.model.Singleton;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Singleton singleton = Singleton.getInstance(new Persona("Carlos","12341234A"));
+		
+		
+		Persona persona1 = singleton.getPersona();
+		Persona persona2 = singleton.getPersona();
+		
+		System.out.println(persona1+" "+persona1.getNombre()+" "+persona1.getDni());
+		System.out.println(persona2+" "+persona2.getNombre()+" "+persona2.getDni());
+	}
+
+}
